341ed872238a3d8b820b05587e80d0a4ba7e7fc5
agordon2019 <agordon2019@cchsraider.net>
2017-09-21 14:12:21 -0400

    Mini Project Completed?

---
+++ MiniProject1/src/MiniProject1.java
@@ -0,0 +1,17 @@
+/*
+ * Aidan Gordon
+ * 9/20/17
+ * This program displays "Victory is mine"
+ * 
+ */
+public class MiniProject1 {
+
+	public static void main(String[] args) {
+		//Displays image
+		System.out.println("//////////////////////");
+		System.out.println("|| Victory is mine! ||");
+		System.out.println("//////////////////////");
+
+	}
+
+}
