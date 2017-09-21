cb896ccf22afd3e5a5c25175f57cc7ba94e7b23a
agordon2019 <agordon2019@cchsraider.net>
2017-09-21 13:41:12 -0400

    initial commit

---
+++ StaticMethodPractice/src/StaticMethodPractice.java
@@ -0,0 +1,24 @@
+
+public class StaticMethodPractice {
+
+	public static void main(String[] args) {
+		wholeLine(); //Top of the top box
+		side(); //Side of top box
+		side();
+		side();
+		wholeLine(); //Bottom of top box
+		wholeLine(); //Top of bottom box
+		side(); //Side of bottom box
+		side();
+		side();
+		wholeLine(); //Bottom of bottom box
+	}
+	//Creates a whole line of asterisks
+	public static void wholeLine(){
+		System.out.println("***********" );
+	}
+	//Creates two asterisks that are spread out for the sides
+	public static void side(){
+		System.out.println("*         *");
+	}
+}
