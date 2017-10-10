1a2bc63a3a8ebd2fda703e4d31596f82dd6f151e
agordon2019 <agordon2019@LAB-206-05.central.cchs>
2017-10-10 08:59:56 -0400

    While Loop Practice Class End 10/10/17

---
+++ WhileLoopPractice/src/WhileLoopPractice.java
@@ -0,0 +1,43 @@
+/* While Loop Practice
+ * Aidan Gordon
+ * 10/10/17
+ * This program accomplishes different tasks by using while loops
+ */
+public class WhileLoopPractice {
+
+	public static void main(String[] args) {
+		//Tests stars method with sample values
+		stars(2);
+		stars(3);
+		stars(10);
+		stars(-1);
+		
+		//Declares variables for each of the tasks
+		int counter = 10;
+		int cntr = 1;
+		int increment = 9;
+		
+		//Prints Liftoff sequence starting at 10 and counting down
+		while (counter >= 2) {
+			System.out.print(counter + "...");
+			counter--;
+		}
+		System.out.println(counter + "...Liftoff!");
+		
+		//Prints out all the odd numbers from 1-100 
+		while (cntr < 100) {
+			System.out.println(cntr);
+			cntr += increment;
+		}
+	}
+	
+	public static void stars(int numberStars) {
+		int counter = 1;
+		
+		while (counter <= numberStars) {
+			System.out.print("*");
+			counter++;
+		}
+		System.out.println();
+	}
+}
