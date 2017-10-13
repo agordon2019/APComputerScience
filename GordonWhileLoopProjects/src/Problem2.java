201b92dd68bdfe7906bb86e787da3584da00d0a2
agordon2019 <agordon2019@LAB-206-05.central.cchs>
2017-10-13 13:59:58 -0400

    Problem 2 Updated End of Class 10/13

--- GordonWhileLoopProjects/src/Problem2.java
+++ GordonWhileLoopProjects/src/Problem2.java
@@ -4,7 +4,7 @@
  * 10/12/17
  * This program allows the user to enter in a population the initial number of
  * organisms, the rate of growth, the number of hours the population takes to 
- * achieve this rate, and the number of hours the population grows,
+ * achieve this rate, and the number of hours the population grows.
  */
 
 import java.util.Scanner; //Imports Scanner for user input
@@ -12,6 +12,37 @@ public class Problem2 {
 
 	public static void main(String[] args) {
 		//Declare variables for calculations and user input
+		int initialPopulation, growthRate, numberHoursAchieved, numberHoursGrows;
+		
+		//Instantiates Scanner
+		Scanner in = new Scanner(System.in);
+		
+		//Prompts user for initial population input
+		System.out.print("Enter the intial number of organisms: ");
+		initialPopulation = in.nextInt();
+		
+		//Prompts user for growth rate input
+		System.out.print("Enter growth rate of the population: ");
+		growthRate = in.nextInt();
+		
+		//Prompts user to input number of hours population takes to achieve growth rate
+		System.out.print("Enter number of hours it takes to achieve the growth rate: ");
+		numberHoursAchieved = in.nextInt();
+		
+		//Prompts user to input number of hours the population grows
+		System.out.print("Enter number of hours the population grows: ");
+		numberHoursGrows = in.nextInt();
+		
+		//Close Scanner
+		in.close();
+		
+		//Calculates predicted population
+		while (numberHoursGrows >= numberHoursAchieved) {
+			initialPopulation *= growthRate;
+			numberHoursGrows -= numberHoursAchieved;
+		}
+		System.out.println(initialPopulation);
+		
 	}
 
 }
