a0335abba6db9c759512b460e01ab2d1be5070a6
aidan <aidan@DESKTOP-2PDGSD8.fios-router.home>
2017-11-12 22:26:24 -0500

    Point Class Homework 11/12/17

---
+++ PointClass/src/Point.java
@@ -0,0 +1,61 @@
+/*
+ * Point Class Method Practice
+ * 11/11/17
+ * Aidan Gordon
+ * This is a class made for any point on the coordinate system
+ */
+
+public class Point {
+	//Declares Instance Variables
+	private double x;
+	private double y;
+	
+	//Default Constructor
+	public Point() {
+		x = 0.0;
+		y = 0.0;
+	}
+	
+	//Constructor With Points
+	public Point(double newX, double newY) {
+		x = newX;
+		y = newY;
+	}
+	
+	//Accessor To Get X Coordinate
+	public double getX() {
+		return x;
+	}
+	
+	//Accessor To Get Y Coordinate
+	public double getY() {
+		return y;
+	}
+	
+	//Mutator to Change X Coordinate
+	public void setX(double newX) {
+		x = newX;
+	}
+	
+	//Mutator to Change Y Coordinate 
+	public void setY(double newY) {
+		y = newY;
+	}
+	
+	//Method that Gets Distance
+	public double getDistance(Point p) {
+		return Math.sqrt((Math.pow(p.getX() - x, 2.0)) + Math.pow((p.getY() - y), 2.0));
+	}
+	
+	//Method That Sets Location
+	public void setLocation(double newX, double newY) {
+		x = newX;
+		y = newY;
+	}
+	
+	//Method that translates a point
+	public void translate(double dx, double dy) {
+		x += dx;
+		y += dy;
+	}
+}
---
+++ PointClass/src/PointClassTest.java
@@ -0,0 +1,43 @@
+/*
+ * Point Class Test 
+ * Aidan Gordon
+ * 11/11/17
+ * This program tests the point class
+ */
+public class PointClassTest {
+
+	public static void main(String[] args) {
+		//Creates two new Points using Point class
+		Point point1 = new Point();
+		Point point2 = new Point(1.0, 2.0);
+		
+		//Tests Accessor for X and Y Coordinates and the Constructor with Points
+		System.out.println(point2.getX() + ", " + point2.getY());
+		
+		//Test Accessor for X and Y Coordinates and the Default Constructor
+		System.out.println(point1.getX() + ", " + point1.getY());
+		
+		//Tests Mutator for X Coordinate
+		point1.setX(2.5);
+		
+		//Tests Mutator for Y Coordinate
+		point1.setY(1.2);
+		
+		//Shows if Mutator worked by printing new Point
+		System.out.println(point1.getX() + ", " + point1.getY());
+		
+		//Tests Distance 
+		System.out.println(point1.getDistance(point2));
+		
+		//Tests Set Location Method
+		point1.setLocation(2.0,  3.0);
+		
+		System.out.println(point1.getX() + ", " + point1.getY());
+		
+		//Tests Translation Method
+		point1.translate(2.0, 1.5);
+		
+		System.out.println(point1.getX() + ", " + point1.getY());
+		
+	}
+}
